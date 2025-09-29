50caf3f8981538323a6dc4e8e45cbd76ba8d3a45
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2025-09-30 04:08:26 +0700

    fix(transactions): show time in transaction list

--- src/features/transactions/components/TransactionList.tsx
+++ src/features/transactions/components/TransactionList.tsx
@@ -6,23 +6,26 @@ interface TransactionListProps {
 
 const TransactionList = ({transactions}: TransactionListProps) => {
   return (
-    <ul className="flex flex-col">
-      {
-        transactions.map(transaction => <li key={transaction.id} className="border-b py-5">
-          <div>
-            <p className="font-semibold capitalize mb-2">{transaction.name}</p>              
-            <div className="flex justify-between items-start mb-2">
-              <div className="flex flex-col gap-1 ">
-                <span className="text-xs text-gray-500">e-wallet number : *******</span>
-                <span className="text-xs text-gray-500">Transaction reference: *******</span>
+    <div className="mt-5">
+      <time dateTime="2023-09-24T06:48:34" className="font-semibold">24 Sep 2023 6:48:34</time>
+      <ul className="flex flex-col">
+        {
+          transactions.map(transaction => <li key={transaction.id} className="border-b py-5">
+            <div>
+              <p className="font-semibold capitalize mb-2">{transaction.name}</p>              
+              <div className="flex justify-between items-start mb-2">
+                <div className="flex flex-col gap-1 ">
+                  <span className="text-xs text-gray-500">e-wallet number : *******</span>
+                  <span className="text-xs text-gray-500">Transaction reference: *******</span>
+                </div>
+                <p className="font-semibold">Amount: <span className="font-semibold">{transaction.amount}</span></p>
               </div>
-              <p className="font-semibold">Amount: <span className="font-semibold">{transaction.amount}</span></p>
+              <span className="text-xs text-gray-500 font-semibold">{transaction.note}</span>
             </div>
-            <span className="text-xs text-gray-500 font-semibold">{transaction.note}</span>
-          </div>
-        </li>)
-      }
-    </ul>
+          </li>)
+        }
+      </ul>
+    </div>
   )
 }
 
