fd27c57a647cd2c7becf7d2da22e6868058094dc
ducothepaladin <lin303393@gmail.com>
2025-12-03 23:48:14 +0630

    fix: minor error

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -112,11 +112,15 @@ export const switchAccount = async (accountId: number) => {
 
 export const mediaUpload = async (data: FormData) => {
   try {
-    const response = await API.post("/media/upload", data, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
+    const response = await API.post(
+      "personal-banking/users/media/upload",
+      data,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     throwError(error);
